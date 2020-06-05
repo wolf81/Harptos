f3f025005d6d15799a6e7bc3f26392578a8bda70
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-06-06 03:02:46 +0700

    Enable generating and publishing docs to Github pages

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCS_DIR="${1}"
+
+if [ "${TRAVIS_BRANCH}" == "master" ]; then
+  	echo "deploy docs"
+
+  	cd "${DOCS_DIR}"
+  	git init
+  	git config user.name "Travis CI"
+	git config user.email "travis@travis-ci.org"
+	git add .
+	git commit --message "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER}"
+	git remote add deploy "https://${GH_TOKEN}@github.com/wolf81/HarptosDocs.git" >/dev/null 2>&1
+	git push --force deploy master >/dev/null 2>&1
+else
+	echo "no docs will be deployed, not on master branch"
+fi
